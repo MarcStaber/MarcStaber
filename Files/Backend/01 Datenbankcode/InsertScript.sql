8b3f032c41ca292becd71cc6e072e03c4d4cd005
MaSta <marc.staber@htlpinkafeld.at>
2024-02-11 19:03:47 +0100

    DB_rfv.sql wurde umbenannt auf 00.DB_rfv_ohne_Daten.sql 01.DB_rfv_mit_Daten.sql und InsertScript.sql wurden hinzugefügt.

---
+++ Files/Backend/01 Datenbankcode/InsertScript.sql
@@ -0,0 +1,8 @@
+INSERT INTO user (email_address, first_name, last_name, `password`, telephone_number, role_id, street, house_number, zip_code, city, country)
+VALUES ("max.mustermann@mustermail.com", "Max", "Mustermann", "muster123", 066412345678, 3, "Musterstraße", 1, 1234, "Musterstadt", "Musterland");
+
+INSERT INTO user (email_address, first_name, last_name, `password`, telephone_number, role_id, street, house_number, zip_code, city, country)
+VALUES ("maxima.mustermann@mustermail.com", "Maxima", "Mustermann", "muster456", 066423456789, 3, "Mustergasse", 2, 4567, "Musterstadt", "Musterland");
+
+INSERT INTO user (email_address, first_name, last_name, `password`, telephone_number, role_id, street, house_number, zip_code, city, country)
+VALUES ("marc.staber@htlpinkafeld.at", "Marc", "Staber", "password", 068012345678, 2, "Musterplatz", 1, 4321, "Musterdorf", "Österreich");
