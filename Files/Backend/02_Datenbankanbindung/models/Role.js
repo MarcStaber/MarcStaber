49634037e8fc3d8dbfd51da3f1b5ceb2a0a7695c
Geckobrother <55572224+Geckobrother@users.noreply.github.com>
2024-03-14 21:49:00 +0100

    currently models
    works

---
+++ Files/Backend/02_Datenbankanbindung/models/Role.js
@@ -0,0 +1,94 @@
+class Role {
+
+    /////////////////////////////////////////////////////////////////////////////////
+    // private class member variables
+    /////////////////////////////////////////////////////////////////////////////////
+    #role_id;
+    #role;
+
+    /////////////////////////////////////////////////////////////////////////////////
+    // constructor
+    /////////////////////////////////////////////////////////////////////////////////
+    constructor(role_id = null, role_name = "") {
+        this.#role_id = role_id;
+        this.#role = role_name;
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////
+    // getter und setter
+    /////////////////////////////////////////////////////////////////////////////////
+    get role_id() {
+        return this.#role_id;
+    }
+    
+    set role_id(role_id) {
+        this.#role_id = role_id;
+    }
+    
+    get role() {
+        return this.#role;
+    }
+    
+    set role(role) {
+        this.#role = role;
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////
+    // to's
+    /////////////////////////////////////////////////////////////////////////////////
+    toString() {
+        return `Role { role_id: "${this.#role_id}", role: "${this.#role}" }`;
+    }
+
+    toArray() {
+        return [this.#role_id, this.#role];
+    }
+
+    toObject() {
+        return {
+            role_id: this.#role_id,
+            role_name: this.#role
+        };
+    }
+
+    toJson() {
+        return JSON.stringify(this.toObject());
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////
+    // static from's
+    /////////////////////////////////////////////////////////////////////////////////
+    static fromArray(array) {
+        return new Role(array[0], array[1]);
+    }
+
+    static fromObject(object) {
+        return new Role(object.role_id, object.role);
+    }
+
+    static fromJson(json) {
+        const obj = JSON.parse(json);
+        return new Role(obj.role_id, obj.role);
+    }
+
+
+
+
+
+
+
+    // war nur zum versuch vielleicht für später
+    // static async getRolesByUserIdAndRoleId(user_id, role_id) {
+
+    //     const sql = 'SELECT * FROM role WHERE user_id =? AND role_id =?';
+    //     const values = [user_id, role_id];
+    //     try {
+    //         const result = await db.pool.query(sql, values);
+    //         return result;
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // }
+
+    
+}
